5bbee3cdf98f102b30f5f097cb999860b3ea2ac5
Nathalia Monteles <nathalia_martinsm@hotmail.com>
2024-06-10 12:14:40 -0300

    CRUD-Categoria

---
+++ src/main/java/com/farmacia/farmacia/model/Categorias.java
@@ -0,0 +1,52 @@
+package com.farmacia.farmacia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categorias {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O campo nome não pode estar vazio")
+	@Size(min = 4, max = 50, message = "O campo descrição precisar ter entre 4 e 50 caracteres")
+	private String nome;
+
+	@NotBlank(message = "O campo Descrição não pode estar vazio")
+	@Size(max = 5000, message = "O campo descrição precisar ter até 5000 caracteres")
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
+
---
+++ src/main/java/com/farmacia/farmacia/repository/CategoriasRepository.java
@@ -0,0 +1,14 @@
+package com.farmacia.farmacia.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.farmacia.farmacia.model.Categorias;
+
+public interface CategoriasRepository extends JpaRepository<Categorias, Long> {
+
+	   public List<Categorias> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
+
+	}
